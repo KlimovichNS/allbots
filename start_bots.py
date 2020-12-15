a1986fcd44fbfb06256983a41db81673a0421933
KlimovichNS <nero.darkside@gmail.com>
2020-12-16 02:52:31 +0300

    b-day bot release

--- start_bots.py
+++ start_bots.py
@@ -3,3 +3,4 @@ import subprocess
 subprocess.Popen(['python3', 'kir.py'])
 subprocess.Popen(['python3', 'platon.py'])
 subprocess.Popen(['python3', 'moyses.py'])
+subprocess.Popen(['python3', 'birthday_bot.py'])
