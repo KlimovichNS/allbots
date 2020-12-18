5e2a408ea25c510d4769cd772272bb1ac7b3bd3b
KlimovichNS <nero.darkside@gmail.com>
2020-12-18 11:30:32 +0300

    cleaning

--- start_bots.py
+++ start_bots.py
@@ -1,6 +1,6 @@
 import subprocess
 
-subprocess.Popen(['python3', 'kir.py'])
-subprocess.Popen(['python3', 'platon.py'])
-subprocess.Popen(['python3', 'moyses.py'])
-subprocess.Popen(['python3', 'birthday_bot.py'])
+subprocess.Popen(['python3', 'kir/kir.py'])
+subprocess.Popen(['python3', 'platon/platon.py'])
+subprocess.Popen(['python3', 'moyses/moyses.py'])
+subprocess.Popen(['python3', 'birthday_bot/birthday_bot.py'])
