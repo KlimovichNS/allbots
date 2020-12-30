cd67d0f1266586f99d1bd293d6beabe3151affa1
KlimovichNS <nero.darkside@gmail.com>
2020-12-30 11:29:49 +0300

    platon ping

--- platon/botping.py
+++ platon/botping.py
@@ -6,12 +6,18 @@ import time
 api_id = 2419610
 api_hash = '687cbe9c453f32b5a56af9e8ef8d7a9d'
 client = TelegramClient('session_name', api_id, api_hash)
-client.start()
-client.run_until_disconnected()
-while True:
-    client.send_message('platon_mbu_bot', '/start')
-    print('send message')
-    for n in range(0,14,1):
-        time.sleep(60)
-        print('#', end='')
+#client.start()
+#client.run_until_disconnected()
+def ping():
+    while True:
+        client.send_message('platon_mbu_bot', '/start')
+        print('send message')
+        for n in range(0,14,1):
+            time.sleep(60)
+            print('#', end='')
 
+if __name__ == '__main__':
+    client.start()
+    ping()
+    client.run_until_disconnected()
+    
