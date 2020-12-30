72b8f068a44e1fd682228ca3d85ff521712e92b6
KlimovichNS <nero.darkside@gmail.com>
2020-12-30 11:19:45 +0300

    telethon fixes

--- platon/botping.py
+++ platon/botping.py
@@ -7,10 +7,11 @@ api_id = 2419610
 api_hash = '687cbe9c453f32b5a56af9e8ef8d7a9d'
 client = TelegramClient('session_name', api_id, api_hash)
 client.start()
-
+client.run_until_disconnected()
 while True:
     client.send_message('platon_mbu_bot', '/start')
     print('send message')
     for n in range(0,14,1):
         time.sleep(60)
         print('#', end='')
+
